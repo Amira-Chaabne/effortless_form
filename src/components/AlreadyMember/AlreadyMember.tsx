0580122b9f2ecf5ff701be8f55d2d1985d1da547
Amira-Chaabane <chaabaneamira.nan@gmail.com>
2023-08-14 23:45:28 +0100

    Handle CTA for both forms

--- src/components/AlreadyMember/AlreadyMember.tsx
+++ src/components/AlreadyMember/AlreadyMember.tsx
@@ -1,13 +1,21 @@
 import { Text } from "@mantine/core";
 import Link from "next/link";
 
-export default function AlreadyMember() {
+export default function AlreadyMember({ flag }: { flag?: boolean }) {
   return (
     <div className="flex justify-end">
-      <Text size="xs">
-        <span>Already a member ? </span>
-        <Link href="#" className="font-bold text-teal-600 hover:text-teal-500 transition-colors">Sign in</Link>
-      </Text>
+      {
+        flag ?
+          <Text size="xs">
+            <span>Already a member? </span>
+            <Link href="#" className="font-bold text-teal-600 hover:text-teal-500 transition-colors">Sign in</Link>
+          </Text> :
+          <Text size="xs">
+            <span>Don't have an account yet? </span>
+            <Link href="#" className="font-bold text-teal-600 hover:text-teal-500 transition-colors">Sign Up Now</Link>
+          </Text>
+      }
+
     </div>
   );
 }
--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -5,7 +5,7 @@ import SignUpForm from "../SignUpForm/SignUpForm";
 export default function SignUp() {
   return (
     <div>
-      <AlreadyMember />
+      <AlreadyMember flag={true} />
       <div className="w-full md:max-w-2xl mx-auto">
         <Text className="text-3xl font-bold my-12">Create an Account</Text>
         <SignUpForm />
