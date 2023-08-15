d2ed6f2dc6e9dfb84c856d9b4bbb1139342673df
Amira-Chaabane <chaabaneamira.nan@gmail.com>
2023-08-15 08:13:03 +0100

    Add animation

--- src/sections/Forms/Forms.tsx
+++ src/sections/Forms/Forms.tsx
@@ -2,6 +2,7 @@ import SignIn from "@/components/SignIn/SignIn";
 import SignUp from "@/components/SignUp/SignUp";
 import { AuthContext } from "@/contexts/AuthContext";
 import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Forms() {
   const [isSigningUp, setIsSigningUp] = useState(true);
@@ -13,7 +14,29 @@ export default function Forms() {
   return (
     <AuthContext.Provider value={{ isSigningUp, switchForm }}>
       <div className="col-span-4 md:col-span-3 p-4 bg-white rounded-md py-12">
-        {isSigningUp ? <SignUp /> : <SignIn />}
+        <AnimatePresence mode="wait">
+          {isSigningUp ? (
+            <motion.div
+              key="signUp"
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -50 }}
+              transition={{ duration: 0.3 }}
+            >
+              <SignUp />
+            </motion.div>
+          ) : (
+            <motion.div
+              key="signIn"
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -50 }}
+              transition={{ duration: 0.3 }}
+            >
+              <SignIn />
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </AuthContext.Provider>
   );
