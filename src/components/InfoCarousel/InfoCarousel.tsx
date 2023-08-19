0d52fb7ad581f1831b8573604d9c8b4e0ada03ed
Amira-Chaabane <chaabaneamira.nan@gmail.com>
2023-08-19 20:11:51 +0100

    Update carousel content

--- src/components/InfoCarousel/InfoCarousel.tsx
+++ src/components/InfoCarousel/InfoCarousel.tsx
@@ -5,9 +5,27 @@ import stars from "../../../public/assets/images/stars_cold_night.jpg";
 import wave from "../../../public/assets/images/wave_in_rocks.jpg";
 
 const images = [
-  { src: road, alt: "calm_peaceful_road" },
-  { src: stars, alt: "stars_cols_night" },
-  { src: wave, alt: "wave_in_rocks" },
+  {
+    src: road,
+    alt: "calm_peaceful_road",
+    title: "Carbon Emissions Surge",
+    description:
+      "Each year, human activities release over 40 billion tons of carbon dioxide into the atmosphere, exacerbating global warming and causing extreme weather events.",
+  },
+  {
+    src: stars,
+    alt: "stars_cols_night",
+    title: "Biodiversity Loss Accelerates",
+    description:
+      "Due to habitat destruction and climate change, species are going extinct at a rate 1,000 times faster than the natural background rate, jeopardizing ecosystems and food security.",
+  },
+  {
+    src: wave,
+    alt: "wave_in_rocks",
+    title: "Melting Polar Ice",
+    description:
+      "Polar ice is disappearing at an alarming rate, with the Arctic losing 12.8% of its ice cover each decade. This contributes to rising sea levels and threatens coastal communities.",
+  },
 ];
 
 export default function InfoCarousel() {
@@ -20,6 +38,10 @@ export default function InfoCarousel() {
         objectFit="cover"
         className="border rounded-md"
       />
+      <div className="absolute bottom-0 left-0 right-0 py-12 px-4 text-white">
+        <h2 className="text-xl font-bold">{image.title}</h2>
+        <p className="text-sm">{image.description}</p>
+      </div>
     </Carousel.Slide>
   ));
 
--- src/sections/Layout.tsx
+++ src/sections/Layout.tsx
@@ -3,7 +3,7 @@ import Forms from "@/sections/Forms/Forms";
 
 export default function Layout() {
   return (
-    <section className="p-4 h-screen grid grid-cols-4 gap-4 container mx-auto">
+    <section className="p-4 h-screen grid grid-cols-4 gap-4 lg:container mx-auto">
       <InfoCarousel />
       <Forms />
     </section>
